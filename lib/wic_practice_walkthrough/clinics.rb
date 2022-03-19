cfe216d5bd4532165b0ccf5b8bf19b5ab7ea321a
Amanda Rodgers <aarodgerswork@gmail.com>
2022-03-19 16:01:44 -0600

    fix deprecated call to URI.open

--- lib/wic_practice_walkthrough/clinics.rb
+++ lib/wic_practice_walkthrough/clinics.rb
@@ -1,7 +1,7 @@
 class WicPracticeWalkthrough::Clinics
     # Scrapes and returns a clinic's: address, url, and phone number.
     def self.scrape_clinic_info(clinic_number)
-        doc = Nokogiri::HTML(open("https://www.wicprograms.org/ci/co-denver"))
+        doc = Nokogiri::HTML(URI.open("https://www.wicprograms.org/ci/co-denver"))
         clinic_info = []
         url = doc.css(".span12")[clinic_number].css("a").attr("href").text
         address_phone = doc.css(".span12")[clinic_number].css("p")[0].text.strip.gsub("\r\n", "")
